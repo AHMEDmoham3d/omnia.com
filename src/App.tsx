68f029e97e66aca69b521fb049ea0584eb451b88
Ahmed Mohamed <7hmedmohamed12@gmail.com>
2025-08-11 13:49:51 +0300

    وصف التعديلات اللي عملتها

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
 import Services from './components/Services';
+import Certificates from './components/Certificates';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import AdminPanel from './components/AdminPanel';
@@ -277,6 +278,7 @@ function App() {
                 <Hero />
                 <About />
                 <Services />
+                <Certificates />
                 <Contact onMessageSent={handleNewMessage} />
                 <Footer />
               </>
